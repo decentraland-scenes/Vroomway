6b952b5406f4c756a3a434612edd2bb0e020c85c
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2024-09-16 10:57:19 -0300

    Fix: Missing Colliders & Fix spawn point
    -Missing colliders on mainInstance
    -Fix: Spawn Point

--- scene/src/instances/main/mainInstance.ts
+++ scene/src/instances/main/mainInstance.ts
@@ -441,6 +441,7 @@ export class MainInstance {
       rotation: Quaternion.fromEulerDegrees(0.0, 179.998, 180.0)
     })
     MeshRenderer.setPlane(this.fuelInfoPoster)
+    MeshCollider.setPlane(this.fuelInfoPoster)
     Material.setPbrMaterial(this.fuelInfoPoster, {
       texture: Material.Texture.Common({
         src: 'https://bafybeibgpc27qayhrp6rnibpqdmbxa3cujflglogwfv53lvm2twt5hpqri.ipfs.nftstorage.link/'
@@ -487,6 +488,7 @@ export class MainInstance {
       rotation: Quaternion.fromEulerDegrees(0.0, 135.0, 180.0)
     })
     MeshRenderer.setPlane(this.soloSprintSign)
+    MeshCollider.setPlane(this.soloSprintSign)
     Material.setPbrMaterial(this.soloSprintSign, {
       texture: Material.Texture.Common({
         src: 'https://bafkreidz7jasew2mjigvh2ja2jm7gd5js6s7mqj5daogwsuerhvrilqmbu.ipfs.nftstorage.link/'
@@ -507,6 +509,7 @@ export class MainInstance {
       rotation: Quaternion.fromEulerDegrees(0.0, 180.0, 180.0)
     })
     MeshRenderer.setPlane(this.dragRaceSign)
+    MeshCollider.setPlane(this.dragRaceSign)
     Material.setPbrMaterial(this.dragRaceSign, {
       texture: Material.Texture.Common({
         src: 'https://bafkreid5gmydkzhkyazy5l6uj5frrwdzicwo6igyeypcktaicraqr4ftk4.ipfs.nftstorage.link/'
@@ -527,6 +530,7 @@ export class MainInstance {
       rotation: Quaternion.fromEulerDegrees(0.0, 180.0, 180.0)
     })
     MeshRenderer.setPlane(this.fuegoCircuitsSign)
+    MeshCollider.setPlane(this.fuegoCircuitsSign)
     Material.setPbrMaterial(this.fuegoCircuitsSign, {
       texture: Material.Texture.Common({
         src: 'https://bafkreigehlbdsuxzcljwbps4kzip4qm3suiu36rtinwwdo34ylawkh5pme.ipfs.nftstorage.link/'
@@ -666,6 +670,7 @@ export class MainInstance {
       rotation: Quaternion.fromEulerDegrees(0.0, 55.0, 180.0)
     })
     MeshRenderer.setPlane(this.puSign)
+    MeshCollider.setPlane(this.puSign)
     Material.setPbrMaterial(this.puSign, {
       texture: Material.Texture.Common({
         src: 'https://bafybeibyecasmxnmhfo5va7zaam6h4c6if7enmxevwq7ufebpqoivksi3i.ipfs.nftstorage.link/'
@@ -890,6 +895,7 @@ export class MainInstance {
       rotation: Quaternion.fromEulerDegrees(360.0, 165.0, 180.0)
     })
     MeshRenderer.setPlane(this.newHere)
+    MeshCollider.setPlane(this.newHere)
     Material.setPbrMaterial(this.newHere, {
       texture: Material.Texture.Common({
         src: 'https://bafkreigmzy4kbncu35ktvh47qox7bk4oae4h5e6gogwnsw5ss7druadtca.ipfs.nftstorage.link/'
